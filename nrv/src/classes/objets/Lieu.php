a81659b9877d5cfd30b623e3be322456c0d206af
Arnexa5 <72047698+Arnexa5@users.noreply.github.com>
2024-11-07 14:29:21 +0100

    Create Lieu.php

---
+++ nrv/src/classes/objets/Lieu.php
@@ -0,0 +1,27 @@
+<?php
+namespace iutnc\nrv\objet ;
+use iutnc\nrv\exception as exception;
+class Lieu{
+    protected String $nom;
+    protected String $adresse;
+    protected Int $place_debout;
+    protected Int $place_assises;
+    protected String $image;
+
+    public function __construct(String $n,String $a,Int $p_d,Int $p_a,String $i){
+        $this -> nom = $n;
+        $this -> adresse = $a;
+        $this -> place_debout = $p_d;
+        $this -> place_assises = $p_a;
+        $this -> image = $i;
+    }
+
+    public function __get($property): mixed
+    {
+        if(property_exists($this, $property)) {
+            return $this->$property;
+        } else {
+            throw new exception\InvalidPropertyNameException($property);
+        }
+    }
+}
