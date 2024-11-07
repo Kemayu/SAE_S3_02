0f4bdcfaaf868ad4cee0b49a0c5fc3a2ca2f69b8
Arnexa5 <72047698+Arnexa5@users.noreply.github.com>
2024-11-07 14:59:16 +0100

    toujours a verifier simple correction de valeur

--- nrv/src/classes/render/RenderSoiree.php
+++ nrv/src/classes/render/RenderSoiree.php
@@ -16,7 +16,7 @@ class RenderSoiree implements renderer{
             <h3 id='nom-soiree'>{$this->soir->nom}</h3>
             <p id='thematique-soiree'>{$this ->soir->thematique} </p>
             <h2>{$this->soir->nom} - {$this->soir->date}</h2>
-            <p>{$this ->soir->horaire} </p>
+            <p>{$this ->soir->heure_debut} </p>
 >>>>>>> 41fb9f9fb6a4de939ef0c305a3a1a97ce5d80495
         </div>
         ";
