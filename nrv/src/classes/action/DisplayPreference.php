501325cfdc4d29a6a841dbe1af372d3b5e970d90
Antonin-M <antonin.mougel1@etu.univ-lorraine.fr>
2024-11-15 17:31:55 +0100

    remove preference

--- nrv/src/classes/action/DisplayPreference.php
+++ nrv/src/classes/action/DisplayPreference.php
@@ -29,11 +29,13 @@ class DisplayPreference extends Action
                 $renderer = new render\RenderSpectacle(new objets\Spectacle((int)$spectacle["ID_SPECTACLE"],$spectacle["TITRE_SPECTACLE"],$spectacle["DESCRIPTION_SPECTACLE"],$spectacle["IMAGE_SPECTACLE"],$spectacle["EXTRAIT_SPECTACLE"],$spectacle["DATE_SPECTACLE"],$spectacle["HORAIRE_SPECTACLE"],$spectacle["DUREE_SPECTACLE"],$spectacle["STYLE_MUSIQUE"],$spectacle["TARIF_SPECTACLE"]));
                 $html.= $renderer->render(1);
                 $html.= <<< END
+                <div class = 'items'>
                 <form method="POST" action="?action=remove-preference">
                 </select>
-                <input type="hidden" name="spectacle" value={(int)$spectacle["ID_SPECTACLE"]}>       
-                <button type="submit">Liker</button>
+                <input type="hidden" name="spectacle" value={$spectacle['ID_SPECTACLE']}>       
+                <button type="submit">Supprimer preference</button>
                 </form>
+                </div>
                 </br></br>"
                 END;
             }
